b7fb47d3b3fc86ffe492c4e334054c3117f43c40
Antoine Dennison <antoine.dennison@gmail.com>
2026-04-30 15:20:58 -0600

    Enable Hardhat test discovery

--- app-web/hardhat.config.js
+++ app-web/hardhat.config.js
@@ -1,5 +1,11 @@
 require("@nomicfoundation/hardhat-toolbox");
 
 module.exports = {
-  solidity: "0.8.24"
+  solidity: "0.8.24",
+  paths: {
+    sources: "./contracts",
+    tests: "./tests",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  }
 };
